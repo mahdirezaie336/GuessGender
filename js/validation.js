dedf97853ca628d187e9fb4f5d11409d7f12acaa
mahdirezaie336 <mahdi.rezaie.336@gmail.com>
2021-12-02 19:01:47 +0330

    Move get names to a function

--- js/validation.js
+++ js/validation.js
@@ -6,9 +6,14 @@ function checkName(event)
     
 }
 
+function getName()
+{
+    return document.getElementById("name-field").value.toLowerCase();
+}
+
 function submit(event)
 {
-    let name = document.getElementById("name-field").value;
+    let name = getName();
     fetch(url_prefix + name)
         .then(response => response.json())
         .then((data) => {
@@ -16,17 +21,20 @@ function submit(event)
                             {
                                 document.getElementById('gender-predict').innerHTML = "None";
                                 document.getElementById('gender-probability').innerHTML = "None";
-                                document.getElementById("result-error").style.visibility = 'visible'
-                                return
+                                document.getElementById("result-error").innerHTML = "Not found!";
+                                document.getElementById("result-error").style.visibility = 'visible';
+                                return;
                             }
-                            document.getElementById("result-error").style.visibility = 'hidden'
+                            document.getElementById("result-error").style.visibility = "hidden";
                             document.getElementById('gender-predict').innerHTML = data['gender'];
                             document.getElementById('gender-probability').innerHTML = data['probability'];
-                        })
+                        });
     
     let saved_gender = localStorage.getItem(name);
     if (saved_gender != null)
         document.getElementById("saved-gender").innerHTML = saved_gender;
+    else
+        document.getElementById("saved-gender").innerHTML = "None";   
 }
 
 function save(event)
@@ -34,7 +42,7 @@ function save(event)
     a = document.getElementsByName("gender-radio");
     let name, gender;
     // TODO: Name validation
-    name = document.getElementById("name-field").value;
+    name = getName();
 
     if (!a[0].checked && !a[1].checked)
     {
@@ -51,11 +59,12 @@ function save(event)
 
     localStorage.removeItem(name);
     localStorage.setItem(name, gender);
+    document.getElementById("saved-gender").innerHTML = gender;
 }
 
 function clear(event)
 {
-    let name = document.getElementById("name-field").value;
+    let name = getName();
     localStorage.removeItem(name);
     document.getElementById("saved-gender").innerHTML = "None";
 }
